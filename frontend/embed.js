cedbe0e86eb2d5580b0097b0cb05557d7302fc22
KyoConnectAI <gorightgoleft@gmail.com>
2025-02-11 20:44:20 +0100

    Update embed.js

--- frontend/embed.js
+++ frontend/embed.js
@@ -1,7 +1,9 @@
 // Simple embed script (added to your website)
 (function() {
   const iframe = document.createElement('iframe');
-  iframe.src = 'https://your-domain.com/chatbot.html';
+  // iframe.src = 'https://your-domain.com/chatbot.html';
+  iframe.src = 'https://kyokaen.github.io/kyoconnectai-chatbot/frontend/chatbot.html';
+  
   iframe.style = 'position: fixed; bottom: 20px; right: 20px; width: 350px; height: 500px; border: none; display: none;';
   
   const toggleButton = document.createElement('div');
